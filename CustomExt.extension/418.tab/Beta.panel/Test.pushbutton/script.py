250951eda453dd4bc9019b5eaace8f43511ff0ef
aliae2425 <noam.carmi80@gmail.com>
2024-03-31 00:46:46 +0100

    add base

--- CustomExt.extension/418.tab/Beta.panel/Test.pushbutton/script.py
+++ CustomExt.extension/418.tab/Beta.panel/Test.pushbutton/script.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-=======
 __author__ = 'Aliae'                               
 __min_revit_ver__ = 2024                                       
 __max_revit_ver__ = 2024                                       
 __highlight__ = 'updated'
 __beta__ = True    
->>>>>>> dev_tool_panel-Reperage
+
