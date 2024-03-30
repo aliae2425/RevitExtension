9624973fa85349052448adf2ed33bc8644db6fc6
aliae2425 <noam.carmi80@gmail.com>
2024-03-30 20:50:56 +0100

    add flooplan picker to 418 tab

--- CustomExt.extension/418.tab/418.panel/col.stack/settings.pushbutton/script.py
+++ CustomExt.extension/418.tab/418.panel/col.stack/settings.pushbutton/script.py
@@ -10,14 +10,14 @@ from pyrevit import DB, HOST_APP, UI, revit, script
 activ_document   = __revit__.ActiveUIDocument.Document
 
 
-def get_section(doc):
+def get_floorPlan(doc):
     return DB.FilteredElementCollector(doc).OfClass(DB.ViewPlan).ToElements()
 
 def selectFloorType(doc):
     ops = []
     floorPlantype = {}
 
-    for section in get_section(doc):
+    for section in get_floorPlan(doc):
         if section.GetTypeId() not in floorPlantype:
             if section.GetTypeId().ToString() != '-1':
                 # floorPlantype[section.GetTypeId().ToString()] = DB.Element.Name.__get__(activ_document.GetElement(section.GetTypeId()))
@@ -32,20 +32,20 @@ def userConfig_ID(doc):
     try:
         # check if the section exist
         user_config.add_section('localplan')
-        ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = int(selectFloorType(doc).ToString())
+        ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = selectFloorType(doc).ToString()
         user_config.save_changes()
         
     except:
         # look if the section exist and the id is set
         if user_config.localplan.get_option('id', "") == "":
-            ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = int(selectFloorType(doc).ToString())
+            ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = selectFloorType(doc).ToString()
             user_config.save_changes()
         # get the id from the config file
         else :
             ID_TYPE_PLAN_REPERAGE = user_config.localplan.get_option('id', "")
     # check if the id is still valid
     if not activ_document.GetElement(DB.ElementId(ID_TYPE_PLAN_REPERAGE)):
-        ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = int(selectFloorType(doc).ToString())
+        ID_TYPE_PLAN_REPERAGE = user_config.localplan.id = selectFloorType(doc).ToString()
         user_config.save_changes()
         
     return ID_TYPE_PLAN_REPERAGE
