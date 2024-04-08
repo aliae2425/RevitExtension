6d97f0b2e1a5aadc8e76097f552185e4ec993530
aliae2425 <noam.carmi80@gmail.com>
2024-04-08 21:45:06 +0200

    script button

---
+++ CustomExt.extension/418.tab/T418.panel/maj.pushbutton/script.py
@@ -0,0 +1,18 @@
+
+# -*- coding: utf-8 -*-
+
+#! python3
+__title__ = 'mise a jour'
+__doc__ = """
+    v0.0.1
+"""
+__author__ = 'Aliae'
+__min_revit_ver__ = 2024                                       
+__max_revit_ver__ = 2025                                       
+__highlight__ = 'new'
+
+doc   = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app   = __revit__.Application
+
+
