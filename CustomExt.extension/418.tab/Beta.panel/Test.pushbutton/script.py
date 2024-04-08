fddb2efb2b929ec2f8eb8b6abdd30e3d6298100c
aliae2425 <noam.carmi80@gmail.com>
2024-04-08 17:30:59 +0200

    empty folder

--- CustomExt.extension/418.tab/Beta.panel/Test.pushbutton/script.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'Aliae'                               
-__min_revit_ver__ = 2024                                       
-__max_revit_ver__ = 2024                                       
-__highlight__ = 'updated'
-__beta__ = True    
