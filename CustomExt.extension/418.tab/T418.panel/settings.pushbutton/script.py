c9c8bc76a6573c0d0aa8e078b637ae5bf0af8a63
aliae2425 <noam.carmi80@gmail.com>
2024-04-08 17:42:09 +0200

    add settings button to the panel

---
+++ CustomExt.extension/418.tab/T418.panel/settings.pushbutton/script.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#! python3
+from pyrevit.userconfig import user_config
+from pyrevit.forms import WPFWindow
+from pyrevit import DB, HOST_APP, UI, revit, script, forms
+
+
+activ_document   = __revit__.ActiveUIDocument.Document
+
