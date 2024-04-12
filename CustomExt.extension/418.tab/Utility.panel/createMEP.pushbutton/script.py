6338c5f423f18cccf7a4c93b1938e031a92a1378
aliae2425 <noam.carmi80@gmail.com>
2024-04-12 23:34:43 +0200

    fixing

---
+++ CustomExt.extension/418.tab/Utility.panel/createMEP.pushbutton/script.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+    
+__title__ = 'Auto present'
+__doc__ = """
+    version : 0.1.0
+    Date : 23.04.2024
+
+"""
+__authors__ = [
+    'Aliae'
+]
+__min_revit_ver__ = 2021                                    
+__max_revit_ver__ = 2025                                      
+__highlight__ = 'new'
