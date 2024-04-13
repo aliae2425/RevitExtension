490835ba528ed8d468aa0c6be69d56ef35fc0077
aliae2425 <noam.carmi80@gmail.com>
2024-04-13 22:46:00 +0200

    rename all levels and get type

--- CustomExt.extension/418.tab/Utility.panel/Autopresent.pushbutton/script.py
+++ CustomExt.extension/418.tab/Utility.panel/Autopresent.pushbutton/script.py
@@ -1,14 +1,49 @@
 # -*- coding: utf-8 -*-
-    
-__title__ = 'Auto present'
-__doc__ = """
-    version : 0.1.0
-    Date : 23.04.2024
 
-"""
-__authors__ = [
-    'Aliae'
-]
-__min_revit_ver__ = 2021                                    
-__max_revit_ver__ = 2025                                      
-__highlight__ = 'new'
+#! python3
+from pyrevit import revit, forms, script
+from pyrevit import DB as DB
+
+
+doc = revit.doc
+uidoc = revit.uidoc
+
+
+class niveau: 
+    def __init__(self):
+        self.niveau = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements()
+        self.sortLevels()       
+    def __repr__(self):
+        for lvl in self.niveau:
+            print("{} at {} | {} \n".format(lvl.Name, lvl.Elevation, lvl.ProjectElevation))
+        return ""   
+    def getLevels(self):
+        return self.niveau   
+    def sortLevels(self):
+        self.niveau = sorted(self.niveau, key=lambda x: x.Elevation)  
+    def renameLevel(self):
+        i = -len([lvl for lvl in self.niveau if lvl.ProjectElevation < -0.00001])
+        for j, lvl in enumerate(self.niveau):
+            lvl.Name = "{:02d}_R{}".format(j,i)
+            i+=1
+        
+class vue:
+    def __init__(self, lvl):
+        self.views = lvl.FindAssociatedPlanViewId()
+    
+    def getViews(self):
+        return self.views
+
+    
+if __name__ == "__main__":
+       
+    planTravail = [PlanType for PlanType in  DB.FilteredElementCollector(doc).OfClass(DB.ViewFamilyType).ToElements()\
+        if DB.Element.Name.__get__(doc.GetElement(PlanType.Id)) == "Travail"]
+    if planTravail:
+        print(planTravail[0].Id)
+    else:
+        forms.alert("Pas de type de travail trouvé.")
+    
+    with revit.Transaction('Rename Levels'):
+        niveau().renameLevel()    
+
