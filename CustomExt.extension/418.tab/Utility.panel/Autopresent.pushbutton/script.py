466a9f09a6b777de7e11f9b10f169c3d4e388307
aliae2425 <noam.carmi80@gmail.com>
2024-04-14 20:58:12 +0200

    base for type selection

--- CustomExt.extension/418.tab/Utility.panel/Autopresent.pushbutton/script.py
+++ CustomExt.extension/418.tab/Utility.panel/Autopresent.pushbutton/script.py
@@ -12,13 +12,11 @@ uidoc = revit.uidoc
 class niveau: 
     def __init__(self):
         self.niveau = DB.FilteredElementCollector(doc).OfCategory(DB.BuiltInCategory.OST_Levels).WhereElementIsNotElementType().ToElements()
-        self.sortLevels()       
+        self.sortLevels()
     def __repr__(self):
         for lvl in self.niveau:
             print("{} at {} | {} \n".format(lvl.Name, lvl.Elevation, lvl.ProjectElevation))
         return ""   
-    def getLevels(self):
-        return self.niveau   
     def sortLevels(self):
         self.niveau = sorted(self.niveau, key=lambda x: x.Elevation)  
     def renameLevel(self):
@@ -26,10 +24,19 @@ class niveau:
         for j, lvl in enumerate(self.niveau):
             lvl.Name = "{:02d}_R{}".format(j,i)
             i+=1
+    def getLevels(self):
+        return self.niveau   
         
 class vue:
     def __init__(self, lvl):
         self.views = lvl.FindAssociatedPlanViewId()
+        
+    def createViews(self, typeId, lvl, Plan):
+        vue = DB.ViewPlan.Create(doc, typeId, lvl.Id)
+        vue.Name = "{}_{}".format(Plan, lvl.Name)
+        # if Plan == "Projet":
+        #     vue.Phas
+        
     
     def getViews(self):
         return self.views
@@ -45,5 +52,15 @@ if __name__ == "__main__":
         forms.alert("Pas de type de travail trouvé.")
     
     with revit.Transaction('Rename Levels'):
-        niveau().renameLevel()    
-
+        niveau().renameLevel()
+    
+    lvl = niveau().getLevels()
+    print(lvl)
+    DefaultType = doc.GetDefaultElementTypeId(DB.ElementTypeGroup.ViewTypeFloorPlan)
+    with revit.Transaction('Create Views'):
+        for l in lvl:
+            Plan = ["Travail", "Projet", "Existant"]
+            for p in Plan:
+            VueTravail = DB.ViewPlan.Create(doc, planTravail[0].Id, l.Id)
+            VueProjet = DB.ViewPlan.Create(doc, DefaultType, l.Id)
+            VueExistant = DB.ViewPlan.Create(doc, DefaultType, l.Id)
