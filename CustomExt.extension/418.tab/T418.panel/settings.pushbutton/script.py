ee1cf8480975ee016b19c07620d56c9d0611db12
aliae2425 <noam.carmi80@gmail.com>
2024-04-18 21:36:44 +0200

    Setting ui intniated

--- CustomExt.extension/418.tab/T418.panel/settings.pushbutton/script.py
+++ CustomExt.extension/418.tab/T418.panel/settings.pushbutton/script.py
@@ -8,3 +8,21 @@ from pyrevit import DB, HOST_APP, UI, revit, script, forms
 
 activ_document   = __revit__.ActiveUIDocument.Document
 
+
+# -*- coding: utf-8 -*-
+
+
+from pyrevit import revit, forms, script
+from pyrevit import DB as DB
+
+import clr
+import wpf
+clr.AddReference('System.Windows.Forms')
+clr.AddReference('IronPython.Wpf')
+from System import Windows
+
+class Myform(Windows.Window):
+    def __init__(self, fileName):
+        wpf.LoadComponent(self, script.get_bundle_file(fileName))
+
+Myform('AutoPresent.xaml').ShowDialog()
