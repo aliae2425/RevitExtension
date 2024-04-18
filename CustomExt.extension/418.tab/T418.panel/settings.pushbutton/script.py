d661288ef77b9b34c626cdfd33cd97db755fe542
aliae2425 <noam.carmi80@gmail.com>
2024-04-18 22:16:11 +0200

    Fixing icon

--- CustomExt.extension/418.tab/T418.panel/settings.pushbutton/script.py
+++ CustomExt.extension/418.tab/T418.panel/settings.pushbutton/script.py
@@ -3,7 +3,7 @@
 #! python3
 from pyrevit.userconfig import user_config
 from pyrevit.forms import WPFWindow
-from pyrevit import DB, HOST_APP, UI, revit, script, forms
+from pyrevit import DB, HOST_APP, UI, revit, script, forms, framework
 
 
 activ_document   = __revit__.ActiveUIDocument.Document
@@ -21,8 +21,19 @@ clr.AddReference('System.Windows.Forms')
 clr.AddReference('IronPython.Wpf')
 from System import Windows
 
-class Myform(Windows.Window):
+class Myform(framework.Windows.Window):
+
+    def set_image_source(self, wpf_element, image_file):
+        """Set source file for image element.
+
+        Args:
+            wpf_element (System.Windows.Controls.Image): xaml image element
+            image_file (str): image file path
+        """
+        WPFWindow.set_image_source_file(wpf_element, image_file)
+        
     def __init__(self, fileName):
         wpf.LoadComponent(self, script.get_bundle_file(fileName))
+        self.set_image_source(self.logo, 'Settings.png')
 
-Myform('AutoPresent.xaml').ShowDialog()
+Myform('Settings.xaml').ShowDialog()
