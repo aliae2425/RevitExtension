5884640d0b19e82b367b02f6c7cd91e113fe6dc7
aliae2425 <noam.carmi80@gmail.com>
2024-04-23 10:30:06 +0200

    loop item

--- CustomExt.extension/418.tab/Family.panel/Browser.pushbutton/script.py
+++ CustomExt.extension/418.tab/Family.panel/Browser.pushbutton/script.py
@@ -21,6 +21,15 @@ clr.AddReference('System.Windows.Forms')
 clr.AddReference('IronPython.Wpf')
 from System import Windows
 
+
+test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+Folder = { 1 :{"Porte" : {"simple", "double", "sous tenture", "tierse"}}\
+    ,2 : {"Mobilier": {"table", "chaise", "armoire", "lit"}}\
+    ,3:{ "Fenetre"}, 4:{"Plante"}\
+    ,5:{"Luminaire" : {"plafonnier", "applique", "suspension", "lampe de bureau"}}\
+    ,6:{ "Sanitaire" : {"lavabo", "wc", "douche", "baignoire"}}
+    }
+
 class Myform(framework.Windows.Window):
 
     def set_image_source(self, wpf_element, image_file):
@@ -35,25 +44,27 @@ class Myform(framework.Windows.Window):
     def __init__(self, fileName):
         wpf.LoadComponent(self, script.get_bundle_file(fileName))
         self.set_image_source(self.logo, 'Logo.png')
+        self.FolderItem = test
         
-        self.set_image_source(self.placeholder, 'UserControl/image.png')
-        self.set_image_source(self.placeholder1, 'UserControl/image.png')
-        self.set_image_source(self.placeholder2, 'UserControl/image.png')
-        self.set_image_source(self.placeholder3, 'UserControl/image.png')
-        self.set_image_source(self.placeholder4, 'UserControl/image.png')
-        self.set_image_source(self.placeholder5, 'UserControl/image.png')
-        self.set_image_source(self.placeholder6, 'UserControl/image.png')
-        self.set_image_source(self.placeholder7, 'UserControl/image.png')
-        self.set_image_source(self.placeholder8, 'UserControl/image.png')
-        self.set_image_source(self.placeholder9, 'UserControl/image.png')
-        self.set_image_source(self.placeholder10, 'UserControl/image.png')
-        self.set_image_source(self.placeholder11, 'UserControl/image.png')
-        self.set_image_source(self.placeholder12, 'UserControl/image.png')
-        self.set_image_source(self.placeholder13, 'UserControl/image.png')
-        self.set_image_source(self.placeholder14, 'UserControl/image.png')
-        self.set_image_source(self.placeholder15, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder1, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder2, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder3, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder4, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder5, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder6, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder7, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder8, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder9, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder10, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder11, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder12, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder13, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder14, 'UserControl/image.png')
+        # self.set_image_source(self.placeholder15, 'UserControl/image.png')
         
-
+    def abort(self, sender, e):
+        self.Close()
 
    
 
